eafbc09effdab844e8dfce8311b8fe54ca23a760
vson <vishals123@gmail.com>
2016-05-02 17:03:00 -0500

    Updated MainActivity with a comment

--- TICTACTOE/app/src/main/java/com/example/avillarreal/test/MainActivity.java
+++ TICTACTOE/app/src/main/java/com/example/avillarreal/test/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
 
     Button a1,a2,a3,b1,b2,b3,c1,c2,c3;
     Button[] button_array;
+    //This is AWESOMEMEEEEEEEE
 
     boolean turn=true;//X=true
     int turnC=0;
